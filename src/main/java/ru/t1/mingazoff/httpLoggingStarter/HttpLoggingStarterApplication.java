55bb1ff7ede21fb6cb5e9a0752b50c1a153fdd2f
Renat Mingazov <mingazofff@gmail.com>
2024-12-04 13:25:00 +0300

    empty Spring Boot App init

---
+++ src/main/java/ru/t1/mingazoff/httpLoggingStarter/HttpLoggingStarterApplication.java
@@ -0,0 +1,13 @@
+package ru.t1.mingazoff.httpLoggingStarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpLoggingStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpLoggingStarterApplication.class, args);
+	}
+
+}
